861be09fbc1477f42b40676215c98a1ff6d872ed
tyhm-chan <tymoteusz5289@gmail.com>
2025-05-07 12:36:13 +0200

    final ajustments

--- Theme/header.php
+++ Theme/header.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <title>Strona</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <meta name="description" content="Strona na praktyki, odtworzony projekt strony: https://symu.co/freebies/templates-4/tanatos-psd-template/">
     <meta name="author" content="Tymoteusz Dombrowski">
+    <title><?php bloginfo('name'); ?> | <?php wp_title(); ?></title>
+    <?php wp_head(); ?>
 </head>
 <script>
     function openMenu() {
@@ -28,49 +28,49 @@
         console.log("closeHire_us()");
    }
 </script>
-<body>
+<body <?php body_class(); ?>>
     <div class="container">
         <div id="menu" onclick="closeMenu()">
-            <div id="menu_content"">
-                <div id="menu_canncel">
-                    <img src="img/cancel.png" alt="cancel" class="invert" onclick="closeMenu()">
-                </div>
-                <div id="menu_items">
-                    <a href="#slider" class="menu_item">HOME</a>
-                    <a href="#team" class="menu_item">ABOUT US</a>
-                    <a href="#services" class="menu_item">SERVICES</a>
-                    <a href="#works" class="menu_item">OUR WORKS</a>
-                    <a href="#contact" class="menu_item">CONTACT</a>
-                    <img src="img/orange_bar.png" alt="bar" id="menu_items_bar">
-                    <div id="menu_social_media">
-                        <a href="https://facebook.com"><img src="img/facebook_logo.png" alt="facebook_logo"></a>
-                        <a href="https://twitter.com"><img src="img/twitter_logo.png" alt="twitter_logo"></a>
-                        <a href="mailto:example@gmail.com"><img src="img/gmail_logo.png" alt="gmail_logo"></a>
-                        <a href="https://dribbble.com"><img src="img/dribbble_logo.png" alt="dribble_logo"></a>
-                        <a href="https://behance.net"><img src="img/behance_logo.png" alt="behance_logo"></a>
+                <div id="menu_content"">
+                    <div id="menu_canncel">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/cancel.png" alt="cancel" class="invert" onclick="closeMenu()">
+                    </div>
+                    <div id="menu_items">
+                        <a href="#slider" class="menu_item">HOME</a>
+                        <a href="#team" class="menu_item">ABOUT US</a>
+                        <a href="#services" class="menu_item">SERVICES</a>
+                        <a href="#works" class="menu_item">OUR WORKS</a>
+                        <a href="#contact" class="menu_item">CONTACT</a>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/orange_bar.png" alt="bar" id="menu_items_bar">
+                        <div id="menu_social_media">
+                            <a href="https://facebook.com"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook_logo.png" alt="facebook_logo"></a>
+                            <a href="https://twitter.com"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter_logo.png" alt="twitter_logo"></a>
+                            <a href="mailto:example@gmail.com"><img src="<?php echo get_template_directory_uri(); ?>/img/gmail_logo.png" alt="gmail_logo"></a>
+                            <a href="https://dribbble.com"><img src="<?php echo get_template_directory_uri(); ?>/img/dribbble_logo.png" alt="dribble_logo"></a>
+                            <a href="https://behance.net"><img src="<?php echo get_template_directory_uri(); ?>/img/behance_logo.png" alt="behance_logo"></a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div id="hire_us"">
-            <div id="hire_us_container"">
-                <div id="hire_us_cancel" onclick="closeHire_us()">X</div>
-                <div id="hire_us_flex">
-                    <p id="hire_us_title">HIRE <span class="orange">US</span></p>
-                    <p id="hire_us_text">Excepteur sint occaecat cupidatat non proident, sunt in culpa <br>qui officia deserunt mollit anim id est laborum</p>
-                    <input type="text" placeholder="Your Email" id="hire_us_email">
-                    <input type="text " placeholder="Subject" id="hire_us_subject">
-                    <textarea placeholder="Your Message" id="hire_us_message"></textarea>
-                    <input type="submit" id="hire_us_button" value="SEND MESSAGE">
+            <div id="hire_us"">
+                <div id="hire_us_container"">
+                    <div id="hire_us_cancel" onclick="closeHire_us()">X</div>
+                    <div id="hire_us_flex">
+                        <p id="hire_us_title">HIRE <span class="orange">US</span></p>
+                        <p id="hire_us_text">Excepteur sint occaecat cupidatat non proident, sunt in culpa <br>qui officia deserunt mollit anim id est laborum</p>
+                        <input type="text" placeholder="Your Email" id="hire_us_email">
+                        <input type="text " placeholder="Subject" id="hire_us_subject">
+                        <textarea placeholder="Your Message" id="hire_us_message"></textarea>
+                        <input type="submit" id="hire_us_button" value="SEND MESSAGE">
+                    </div>
                 </div>
             </div>
-        </div>
-        <header>
-            <p id="header_title">
-                Tanatos<b><span class="orange">Studio</span></b>
-            </p>
-            <p id="header_menu" onclick="openMenu()">
-                Menu &nbsp;&nbsp;
-                <img class="invert" src="img/hamburger.png" alt="hamburger">
-            </p>
-        </header>
+            <header>
+                <p id="header_title">
+                    Tanatos<b><span class="orange">Studio</span></b>
+                </p>
+                <p id="header_menu" onclick="openMenu()">
+                    Menu &nbsp;&nbsp;
+                    <img class="invert" src="<?php echo get_template_directory_uri(); ?>/img/hamburger.png" alt="hamburger">
+                </p>
+            </header>
