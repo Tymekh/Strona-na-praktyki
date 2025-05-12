0d48ad4d75637ea951ea5745a3118153222af86b
tyhm-chan <tymoteusz5289@gmail.com>
2025-05-12 12:37:15 +0200

    updated theme

--- Theme/footer.php
+++ Theme/footer.php
@@ -1,9 +1,10 @@
-<footer>
-            <p>&copy;2015 COPYRIGHT TANATOS STUDIO</p>
-            <p>DESIGNED BY <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" id="logo"></p>
+        <footer>
+            <div id="footer_container">
+                <p>&copy;&nbsp;2015 COPYRIGHT DESIGN STUDIO</p>
+                <p>DESIGNED BY <src="<?php echo get_template_directory_uri(); ?>/src="img/logo.png" alt="logo" id="logo"></p>
+            </div>
         </footer>
     </div>
-    <div class="side"></div>
     <?php wp_footer(); ?>
 </body>
 </html>
