c5b5e6abf9e2badfe436c6398abe309298b262c4
tyhm-chan <tymoteusz5289@gmail.com>
2025-05-14 12:39:46 +0200

    fixed logo in footer

--- Theme/footer.php
+++ Theme/footer.php
@@ -1,7 +1,9 @@
         <footer>
             <div id="footer_container">
                 <p>&copy;&nbsp;2015 COPYRIGHT DESIGN STUDIO</p>
-                <p>DESIGNED BY <src="<?php echo get_template_directory_uri(); ?>/src="img/logo.png" alt="logo" id="logo"></p>
+                <p>DESIGNED BY 
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" id="logo">
+                </p>
             </div>
         </footer>
     </div>
